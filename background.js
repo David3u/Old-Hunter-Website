8de2d8075690982a9f132866d27ca84e5d981912
Dg <da6d3u@gmail.com>
2024-09-18 19:47:20 -0400

    Clean up code

--- background.js
+++ background.js
@@ -4,50 +4,48 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 		const url = new URL(tab.url);
 
 		// Is Hunter Website?
-		if (url.hostname === 'hunterschools.myschoolapp.com') {
-			// Is new style?
-			if (url.pathname.includes("lms-assignment")) {
-				// Is assignment?
-				if (url.pathname.includes("assignment-student-view")) {
-					const identifier = url.pathname.split('/').pop();
-					fetch(`https://hunterschools.myschoolapp.com/api/DataDirect/AssignmentCenterAssignments/?format=json&filter=1&dateStart=8%2F1%2F2024&dateEnd=10%2F1%2F2029&persona=2&statusList=&sectionList=`, {
-						method: 'GET',
-						//mode: 'no-cors',
-						headers: {
-							'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-							'Referer': 'https://google.com',
-						}})
-					.then(response => {
-						if (!response.ok) {
-							throw new Error(`HTTP error! Status: ${response.status}`);
-						}
-						return response.text(); // Use text() first to inspect the raw response
-						})
-						
-					.then(data => {
-						data = JSON.parse(data);
+		if (url.hostname != 'hunterschools.myschoolapp.com') {
+			return;
+		}
 
-						console.log(identifier)
-					// Find the item with the specific key
-						const item = data.find(item => item.assignment_index_id == identifier);
-			
-						if (item) {
-							const newLink = `https://hunterschools.myschoolapp.com/app/student#assignmentdetail/${item.assignment_id}/${item.assignment_index_id}/0/studentmyday--assignment-center`;
-
-							chrome.tabs.update(tabId, { url: newLink });
-						} else {
-							console.log("No item")
-						}
-					})
-					.catch(error => console.error('Error fetching data:', error));
-				} else if (url.pathname.includes("assignment-center")) {
-					chrome.tabs.update(tabId, { url: "https://hunterschools.myschoolapp.com/app/student#studentmyday/assignment-center" });
+		// Is new style?
+		if (!(url.pathname.includes("lms-assignment"))) {
+			return;
+		}
 				
-				}
+		// Is assignment?
+		if (url.pathname.includes("assignment-student-view")) {
+			const identifier = url.pathname.split('/').pop();
+			fetch(`https://hunterschools.myschoolapp.com/api/DataDirect/AssignmentCenterAssignments/?format=json&filter=1&dateStart=8%2F1%2F2024&dateEnd=10%2F1%2F2029&persona=2&statusList=&sectionList=`, {
+				method: 'GET',
+				//mode: 'no-cors',
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+					'Referer': 'https://google.com',
+				}})
+			.then(response => {
+				if (!response.ok) { throw new Error(`HTTP error! Status: ${response.status}`); }
+				return response.text(); })
+			.then(data => {
+				data = JSON.parse(data);
 
-			}
-		} else {
-			console.error('Not website ' + url.hostname );
+				// Find the assignment object
+				const item = data.find(item => item.assignment_index_id == identifier);
+	
+				if (item) {
+					const newLink = `https://hunterschools.myschoolapp.com/app/student#assignmentdetail/${item.assignment_id}/${item.assignment_index_id}/0/studentmyday--assignment-center`;
+
+					chrome.tabs.update(tabId, { url: newLink });
+				} else {
+					console.log("WTF!! Assignment doesn't exist!")
+				}
+			})
+			.catch(error => console.error('Error fetching data:', error));
+
+		// Is Assingment Center?	
+		} else if (url.pathname.includes("assignment-center")) {
+			chrome.tabs.update(tabId, { url: "https://hunterschools.myschoolapp.com/app/student#studentmyday/assignment-center" });
+		
 		}
 	}
 });
